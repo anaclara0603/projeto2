ab3d09c33f7cf417ffaeacee1b26e5fae1a9acf9
= <=>
2023-05-12 13:55:06 -0300

    dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,79 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const produtos = ref([
   {
     id: 1,
     nome: 'Camiseta',
-    preco: 49.90,
-    quantidade: 1
+    preco: 49.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 2,
     nome: 'Calça',
-    preco: 99.90,
-    quantidade: 1
+    preco: 99.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 3,
     nome: 'Meia',
-    preco: 9.90,
-    quantidade: 1
+    preco: 9.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 4,
     nome: 'Sapato',
-    preco: 199.90,
-    quantidade: 1
+    preco: 199.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 5,
     nome: 'Boné',
-    preco: 29.90,
-    quantidade: 1
+    preco: 29.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 6,
     nome: 'Óculos',
-    preco: 99.90,
-    quantidade: 1
+    preco: 99.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 7,
     nome: 'Relógio',
-    preco: 299.90,
-    quantidade: 1
+    preco: 299.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 8,
     nome: 'Bermuda',
-    preco: 79.90,
-    quantidade: 1
+    preco: 79.9,
+    quantidade: 1,
+    tamanho: ''
   },
   {
     id: 9,
     nome: 'Cueca',
-    preco: 19.90,
-    quantidade: 1
+    preco: 19.9,
+    quantidade: 1,
+    tamanho: ''
   }
 ])
 let enviar = ref(false)
 let carrinho = ref([])
 
 function addcarrinho(item) {
-  const index = carrinho.value.findIndex(x => x.codigo === item.id)
+  const index = carrinho.value.findIndex((x) => x.codigo === item.id)
   if (index > -1) {
     alert('item já está no carrinho')
-  }
-  else {
+  } else {
     carrinho.value.push({
       codigo: item.id,
       nome: item.nome,
@@ -75,7 +83,6 @@ function addcarrinho(item) {
     })
     calculacarrinho()
   }
-
 }
 
 let valorcarrinho = ref(0)
@@ -92,7 +99,9 @@ function addquant(index) {
 }
 
 function removerquant(index) {
-  if (produtos.value[index].quantidade > 1) { produtos.value[index].quantidade-- }
+  if (produtos.value[index].quantidade > 1) {
+    produtos.value[index].quantidade--
+  }
   // somaitem.value = item.preco * item.quantidade
 }
 
@@ -105,76 +114,98 @@ function remover(index) {
   calculacarrinho()
 }
 function vercarrinho() {
-  if(carrinho.value < [0]){
+  if (carrinho.value < [0]) {
     alert('carrinho vazio')
-  }
-  else{
+  } else {
     enviar.value = !enviar.value
   }
 }
 
-const avf = (value) => "R$ " + value.toFixed(2).replace('.', ',')
-
+const avf = (value) => 'R$ ' + value.toFixed(2).replace('.', ',')
 </script>
 
 <template>
+  <h1>MIAUMIAUMIAU</h1>
+   <div class="dropdown">
+    <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+      aria-haspopup="true" aria-expanded="false" @click="vercarrinho">Ver carrinho</button>
+    <ul class="dropdown-menu carrinho" v-if="enviar">
+      <li v-for="(item, index) in carrinho" :key="index">
+        <p>Produto: {{ item.nome }}</p>
+        <p>Preço:{{ avf(item.preco) }}</p>
+        <p>Quantidade: {{ item.quantidade }}</p>
+        <button class="btn btn-dark btn-sm" @click="remover(index)">remover</button>
+      </li>
+      <p class="dropdown-item ">Valor total: {{ avf(valorcarrinho) }}</p>
+      <button class="btn btn-dark btn-sm dropdown-item" @click="limpacarrinho()">limpa carrinho</button>
+      <button class="btn btn-dark btn-sm dropdown-item" @click="enviar = !enviar">Fechar Carrinho</button>
+    </ul>
+  </div>
   <div class="produtos">
-    <h1>MIAUMIAUMIAU</h1>
     <ul>
       <li v-for="(item, index) in produtos" :key="index">
         <p>Item: {{ item.nome }}</p>
         <p>Valor: {{ avf(item.preco) }}</p>
         <p>Quantidade: {{ item.quantidade }}</p>
-        <p><button class="btn btn-dark btn-sm" @click="addcarrinho(item)">adicionar ao carrinho</button>
+        <p>
+          <button class="btn btn-dark btn-sm" @click="addcarrinho(item)">adicionar ao carrinho</button>
           <button class="btn btn-dark btn-sm" @click="addquant(item.id - 1)">+</button>
           <button class="btn btn-dark btn-sm" @click="removerquant(index)">-</button>
         </p>
       </li>
     </ul>
-    <button class="btn btn-dark btn-lg" @click="vercarrinho()">Ver carrinho</button>
-  </div>
-  <div v-if="enviar" class="carrinho">
-    <ul>
-      <li v-for="(item, index) in carrinho" :key="index">
-        <p>Produto: {{ item.nome }}</p>
-        <p>Preço:{{ avf(item.preco) }}</p>
-        <p>Quantidade: {{ item.quantidade }}</p>
-        <p>valor total {{ avf(item.valortotal) }}</p>
-        <button class="btn btn-dark btn-sm" @click="remover(index)">remover</button>
-      </li>
-    </ul>
-    <p>Valor total: {{ avf(valorcarrinho) }}</p>
-    <button class="btn btn-dark btn-sm" @click="limpacarrinho()">limpa carrinho</button> 
-    <button class="btn btn-dark btn-sm" @click="enviar = !enviar">Fechar Carrinho</button>
-    aaa
   </div>
 </template>
 
 <style scoped>
 ul {
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-align-items: center;
-justify-content: space-around;
-width: 90%;
-margin: 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
 }
-li{
+
+li {
   justify-content: center;
   align-items: center;
   text-align: center;
   list-style: none;
 }
-button{
+
+button {
   margin: 3px;
 }
-p{
+
+p {
   font-size: large;
 }
-.produtos{
-  justify-content: center;
+
+.produtos {
+  width: 900px;
+  display: flex;
   align-items: center;
   text-align: center;
+  margin: 50px auto;
+  padding: 15px;
 }
+
+.carrinho {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 50%;
+  align-items: center;
+  text-align: center;
+  padding: 5px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+}
+.dropdown{
+  background-color: pink;
+}
+
 </style>
