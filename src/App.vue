9f618490466f8717020b79645c5ee56902eff256
= <=>
2023-05-12 14:26:34 -0300

    final

--- src/App.vue
+++ src/App.vue
@@ -7,67 +7,60 @@ const produtos = ref([
     nome: 'Camiseta',
     preco: 49.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 2,
     nome: 'Calça',
     preco: 99.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 3,
     nome: 'Meia',
     preco: 9.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 4,
     nome: 'Sapato',
     preco: 199.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 5,
     nome: 'Boné',
     preco: 29.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 6,
     nome: 'Óculos',
     preco: 99.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 7,
     nome: 'Relógio',
     preco: 299.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 8,
     nome: 'Bermuda',
     preco: 79.9,
     quantidade: 1,
-    tamanho: ''
   },
   {
     id: 9,
     nome: 'Cueca',
     preco: 19.9,
     quantidade: 1,
-    tamanho: ''
   }
 ])
+const avf = (value) => 'R$ ' + value.toFixed(2).replace('.', ',')
 let enviar = ref(false)
 let carrinho = ref([])
+let valorcarrinho = ref(0)
 
 function addcarrinho(item) {
   const index = carrinho.value.findIndex((x) => x.codigo === item.id)
@@ -85,7 +78,6 @@ function addcarrinho(item) {
   }
 }
 
-let valorcarrinho = ref(0)
 function calculacarrinho() {
   valorcarrinho.value = 0
   for (let index = 0; index < carrinho.value.length; index++) {
@@ -95,7 +87,6 @@ function calculacarrinho() {
 
 function addquant(index) {
   produtos.value[index].quantidade++
-  // somaitem.value = item.preco * item.quantidade
 }
 
 function removerquant(index) {
@@ -121,25 +112,28 @@ function vercarrinho() {
   }
 }
 
-const avf = (value) => 'R$ ' + value.toFixed(2).replace('.', ',')
 </script>
 
 <template>
   <h1>MIAUMIAUMIAU</h1>
-   <div class="dropdown">
+  <div class="dropdown">
     <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
       aria-haspopup="true" aria-expanded="false" @click="vercarrinho">Ver carrinho</button>
-    <ul class="dropdown-menu carrinho" v-if="enviar">
-      <li v-for="(item, index) in carrinho" :key="index">
-        <p>Produto: {{ item.nome }}</p>
-        <p>Preço:{{ avf(item.preco) }}</p>
-        <p>Quantidade: {{ item.quantidade }}</p>
-        <button class="btn btn-dark btn-sm" @click="remover(index)">remover</button>
-      </li>
-      <p class="dropdown-item ">Valor total: {{ avf(valorcarrinho) }}</p>
-      <button class="btn btn-dark btn-sm dropdown-item" @click="limpacarrinho()">limpa carrinho</button>
-      <button class="btn btn-dark btn-sm dropdown-item" @click="enviar = !enviar">Fechar Carrinho</button>
-    </ul>
+    <div class="dropdown-menu carrinho" v-if="enviar">
+      <div class="items-carrinho">
+        <div v-for="(item, index) in carrinho" :key="index" class="item-carrinho">
+          <p>Produto: {{ item.nome }}</p>
+          <p>Preço:{{ avf(item.preco) }}</p>
+          <p>Quantidade: {{ item.quantidade }}</p>
+          <button class="btn btn-dark btn-sm" @click="remover(index)">remover</button>
+        </div>
+      </div>
+      <div class="resumo-carrinho">
+        <p class="dropdown-item-text">Valor total: {{ avf(valorcarrinho) }}</p>
+        <button class="btn btn-dark btn-sm dropdown-item" @click="limpacarrinho()">limpa carrinho</button>
+        <button class="btn btn-dark btn-sm dropdown-item" @click="enviar = !enviar">Fechar Carrinho</button>
+      </div>
+    </div>
   </div>
   <div class="produtos">
     <ul>
@@ -193,19 +187,32 @@ p {
 .carrinho {
   position: absolute;
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  width: 50%;
-  align-items: center;
-  text-align: center;
-  padding: 5px;
+  flex-direction: column;
+  margin: 0 auto;
   left: 0;
   right: 0;
+  width: 50%;
+  align-items: center;
+}
+
+.items-carrinho {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  padding: 5px;
   margin-left: auto;
   margin-right: auto;
 }
-.dropdown{
-  background-color: pink;
+
+.item-carrinho{
+  margin: 10px;
 }
 
+
+.dropdown {
+  background-color: pink;
+}
 </style>
