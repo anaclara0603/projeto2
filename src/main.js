b48e11be2e8f2c3235e6ea19d19411bfe1f30d09
Ana <106554582+anamiau@users.noreply.github.com>
2023-05-12 09:43:41 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-// import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+import './assets/main.css'
 
 createApp(App).mount('#app')
